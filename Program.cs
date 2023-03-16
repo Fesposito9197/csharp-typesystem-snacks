966df2bda18c3ce3ccba4e23009be8bf50f4f53c
Fesposito9197 <kekkoesp8@gmail.com>
2023-03-16 14:18:36 +0100

    aggiunge snack 1 completato

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+snack1();
+
+
+
+void snack1()
+{
+    Console.WriteLine("Inserisci il primo numero: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Inserisci il secondo numero: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    if (num1 > num2)
+    {
+        Console.WriteLine("il numero maggiore e: {0}" ,num1);
+    }
+    else
+    {
+        Console.WriteLine("il numero maggiore e: {0}", num2);
+    }
+}
